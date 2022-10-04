0aabf45085dd9ed14709291bde0143925e28d049
Matheus Barbosa e Silva <bs.matheus@protonmail.com>
2022-10-04 00:17:01 -0300

    🚀 Deploy: demo spring application

---
+++ src/test/java/br/com/bb/ada/projetofinal/ProjetoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bb.ada.projetofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
